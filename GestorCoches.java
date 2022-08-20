f8618de19f83ba728c6fbe9a7703abf06a98862b
fbeltrame <97996900+Shinji7847@users.noreply.github.com>
2022-08-20 12:10:42 -0300

    Update GestorCoches.java

--- GestorCoches.java
+++ GestorCoches.java
@@ -5,28 +5,31 @@ import java.util.ArrayList;
 public class GestorCoches {
     CochesDBMemoria cochesDB = new CochesDBMemoria();
 
-    public String listarCoches(ArrayList coches) {
-        for(Object c : coches)
-            return c.toString();
-
-        return null;
+    public void listarCoches(ArrayList coches) {
+        System.out.println("Listado de coches");
+        for(int i = 0; i < coches.size(); i++){
+            System.out.println("El coche en posición " + i + " es " + coches.get(i));
+        }
     }
 
-    public Coche obtenerCoche(Coche coche) {
+    public Coche obtenerCoche(Coche coche, String modelo) {
         if (coche instanceof CocheElectrico) {
             Coche cocheElectrico = new CocheElectrico();
+            coche.modelo = modelo;
 
             return cochesDB.buscar(cocheElectrico);
         }
 
         if (coche instanceof CocheDeCombustible) {
             Coche cocheDeCombustible = new CocheDeCombustible();
+            coche.modelo = modelo;
 
             return cochesDB.buscar(cocheDeCombustible);
         }
 
         if (coche instanceof CocheHibrido){
             Coche cocheHibrido = new CocheHibrido();
+            coche.modelo = modelo;
 
             return cochesDB.buscar(cocheHibrido);
         }
@@ -35,17 +38,19 @@ public class GestorCoches {
     }
 
     public void crearCoche(Coche coche) {
-        if (cochesDB.buscar(coche) != null) {
-            System.out.println("El coche ya existe");
-        }else {
-
-            cochesDB.crear(coche);
+        if(cochesDB.buscar(coche) != null){
+            return;
         }
+
+        cochesDB.crear(coche);
     }
 
     public void borrarCoche(Coche coche) {
-        if (cochesDB.buscar(coche) != null) {
-            cochesDB.borrar(coche);
+        if(cochesDB.buscar(coche) == null){
+            System.out.println("El coche que quiere borrar no existe");
         }
+
+        cochesDB.borrar(coche);
     }
 }
+
