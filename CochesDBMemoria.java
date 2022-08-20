4aa0487efb74351014c3e529378088f8bb8eaf73
fbeltrame <97996900+Shinji7847@users.noreply.github.com>
2022-08-20 12:09:33 -0300

    Update CochesDBMemoria.java

--- CochesDBMemoria.java
+++ CochesDBMemoria.java
@@ -2,42 +2,55 @@ package open_bootcamp.Coche_SOL_sesion24;
 
 import java.util.ArrayList;
 
-public class CochesDBMemoria {
-    ArrayList<Coche> coches = new ArrayList<>();
+public class GestorCoches {
+    CochesDBMemoria cochesDB = new CochesDBMemoria();
 
-
-    public CochesDBMemoria(){}
-
-    public ArrayList<Coche> listar(Coche coche) {
-            return coches;
-    }
-
-    public Coche buscar(Coche coche) {
-       for (Coche cocheActual : coches) {
-           if (cocheActual.patente.equalsIgnoreCase(coche.patente)) {
-                return cocheActual;
-           }
-       }
-       return null;
-    }
-
-    public void crear(Coche coche){
-
-        for (Coche cocheActual : coches) {
-            if (cocheActual.patente.equalsIgnoreCase(coche.patente)) {
-                System.out.println("El coche ya fue creado");
-            }else {
-                coches.add(coche);
-            }
+    public void listarCoches(ArrayList coches) {
+        System.out.println("Listado de coches");
+        for(int i = 0; i < coches.size(); i++){
+            System.out.println("El coche en posición " + i + " es " + coches.get(i));
         }
     }
 
-    public void borrar(Coche coche) {
-        for (int i = 0; i < coches.size(); i++) {
-            Coche cocheElectrico = coches.get(i);
-            if (cocheElectrico.patente.equalsIgnoreCase(cocheElectrico.patente.toLowerCase())) {
-                coches.remove(i);
-            }
+    public Coche obtenerCoche(Coche coche, String modelo) {
+        if (coche instanceof CocheElectrico) {
+            Coche cocheElectrico = new CocheElectrico();
+            coche.modelo = modelo;
+
+            return cochesDB.buscar(cocheElectrico);
         }
+
+        if (coche instanceof CocheDeCombustible) {
+            Coche cocheDeCombustible = new CocheDeCombustible();
+            coche.modelo = modelo;
+
+            return cochesDB.buscar(cocheDeCombustible);
+        }
+
+        if (coche instanceof CocheHibrido){
+            Coche cocheHibrido = new CocheHibrido();
+            coche.modelo = modelo;
+
+            return cochesDB.buscar(cocheHibrido);
+        }
+
+        return null;
+    }
+
+    public void crearCoche(Coche coche) {
+        if(cochesDB.buscar(coche) != null){
+            return;
+        }
+
+        cochesDB.crear(coche);
+    }
+
+    public void borrarCoche(Coche coche) {
+        if(cochesDB.buscar(coche) == null){
+            System.out.println("El coche que quiere borrar no existe");
+        }
+
+        cochesDB.borrar(coche);
     }
 }
+
